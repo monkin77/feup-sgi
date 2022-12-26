1395263ae421c15da7bfd23aec9778159dcecb2b
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-12-26 02:46:07 +0000

    Adds new type of animation, for the checkers game

---
+++ tp3/scenes/model/CheckersAnimation.js
@@ -0,0 +1,28 @@
+export default class CheckersAnimation extends MyAnimation {
+    constructor(sequence) {
+        super();
+        this._sequence = sequence;
+    }
+
+    update(t) {
+        // TODO: Update animation
+    }
+
+    apply() {
+        // TODO: Display animation
+    }
+
+    /**
+     * Starts the animation
+     */
+    start() {
+        this.started = true;
+    }
+
+    /**
+     * Resets the animation
+     */
+    reset() {
+        // TODO: Reset animation
+    }
+}
