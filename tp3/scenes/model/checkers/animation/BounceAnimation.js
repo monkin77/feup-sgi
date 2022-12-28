2e7d49928757ab81af418d55a2afa23e19781363
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-12-28 19:23:08 +0000

    Jumping animation when capturing pieces

--- tp3/scenes/model/checkers/animation/BounceAnimation.js
+++ tp3/scenes/model/checkers/animation/BounceAnimation.js
@@ -1,8 +1,11 @@
 import { KeyFrame } from "../../KeyFrame.js";
 import CheckersAnimation from "./CheckersAnimation.js";
 
-const BOUNCE_ANIMATION_DURATION = 1000;
+const BOUNCE_ANIMATION_DURATION = 1250;
+const TIME_FACTOR = BOUNCE_ANIMATION_DURATION / 1000;
 const BOUNCE_ANIMATION_ID = "bounce-animation-a-totally-random-string";
+const NUM_KEYFRAMES = 100;
+const QUADRATIC_SCALE_FACTOR = 15;
 
 export default class BounceAnimation extends CheckersAnimation {
     constructor(scene, initialPos, finalPos) {
@@ -19,10 +22,22 @@ export default class BounceAnimation extends CheckersAnimation {
         // get translation from initialPos to finalPos
         const boardTranslation = vec3.sub(vec3.create(), this._finalPos, this._initialPos);
         const realTranslation = [boardTranslation[0], -boardTranslation[2], boardTranslation[1]];
-        return [
-            this.buildKeyFrame(0, [0, 0, 0], [0, 0, 0], [1, 1, 1]),
-            this.buildKeyFrame(1, realTranslation, [0, 0, 0], [1, 1, 1]),
-        ];
+
+        const keyframes = [];
+        for (let i = 0; i < NUM_KEYFRAMES; i++) {
+            const t = (i / NUM_KEYFRAMES) * TIME_FACTOR;
+            const y = QUADRATIC_SCALE_FACTOR * t * (TIME_FACTOR - t);
+            const yTranslation = [0, 0, y];
+
+            const scaledRealTranslation = vec3.lerp(vec3.create(), [0, 0, 0], realTranslation, t / TIME_FACTOR);
+            const translation = vec3.add(vec3.create(), yTranslation, scaledRealTranslation);
+            const keyframe = this.buildKeyFrame(t, translation, [0, 0, 0], [1, 1, 1]);
+            keyframes.push(keyframe);
+        }
+
+        keyframes.push(this.buildKeyFrame(BOUNCE_ANIMATION_DURATION / 1000, realTranslation, [0, 0, 0], [1, 1, 1]));
+
+        return keyframes;
     }
 
     getKeyFrameID() {
