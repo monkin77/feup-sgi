c8c3073680348bf6bf7d0cb77c52777f376e0e23
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-12-29 03:31:02 +0000

    Finished players' scoreboards

--- tp3/scenes/model/checkers/MyScoreBoard.js
+++ tp3/scenes/model/checkers/MyScoreBoard.js
@@ -1,3 +1,4 @@
+import { CGFtexture } from "../../../../lib/CGF.js";
 import { MyRectangle } from "../../../primitives/MyRectangle.js";
 import { displayText } from "../../../utils/font.js";
 
@@ -18,16 +19,36 @@ export default class MyScoreBoard {
         this._score2 = 0;
 
         this._card = new MyRectangle(scene, "score-board-card", 0, sideLength / 3, 0, sideLength / 5);
+        this._texture = new CGFtexture(this._scene, "scenes/images/board/storage_wood.jpg");
     }
 
     /**
      * Displays the score board
      */
     display() {
+        this.displayScoreBoard(true);
+        this.displayScoreBoard(false);
+    }
+
+    /**
+     * Displays a score board at either side of the board
+     */
+    displayScoreBoard(isWhitePerspective) {
+        this._boardMaterial.setTexture(this._texture);
+        this._boardMaterial.setTextureWrap("REPEAT", "REPEAT");
+        this._boardMaterial.apply();
+
         this._scene.pushMatrix();
 
-        this._scene.translate(0, this._sideLength, 0);
-        this._scene.rotate(Math.PI / 2, 1, 0, 0);
+        if (isWhitePerspective) {
+            this._scene.translate(0, this._sideLength, 0);
+            this._scene.rotate(Math.PI / 2, 1, 0, 0);
+        } else {
+            this._scene.translate(this._sideLength, 0, 0);
+            this._scene.rotate(Math.PI / 2, 1, 0, 0);
+            this._scene.rotate(Math.PI, 0, 1, 0);
+        }
+
         this._card.display();
         this._scene.translate(0, 0, 0.1);
 
@@ -52,7 +73,7 @@ export default class MyScoreBoard {
      * Increases the score of the given player
      * @param {number} player
      */
-    increaseScore(player) {
+    increaseScore(player) { // TODO: Increase score after a player wins
         player === 1 ? this._score1++ : this._score2++;
     }
 }
