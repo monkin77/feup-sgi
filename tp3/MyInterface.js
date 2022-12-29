f9d7ad2803fba93c673863460807c2dc4f7168e4
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-12-29 20:56:53 +0000

    Fixed bug where active camera was not being updated

--- tp3/MyInterface.js
+++ tp3/MyInterface.js
@@ -46,7 +46,7 @@ export class MyInterface extends CGFinterface {
         }
 
         // Selected View
-        this.gui.add(this.scene, 'selectedView', this.scene.viewsSelector).name('Active Camera').onChange(this.scene.onViewChange);
+        this.gui.add(this.scene, 'selectedView', this.scene.viewsSelector).name('Active Camera').onChange(this.scene.onViewChange).listen();
 
         this.gui.add(this.scene, 'rotateAutomatically').name("Auto Rotate").onChange(this.scene.onToggleAutoRotate);
         this.gui.add(this.scene, 'undo').name('Undo');
